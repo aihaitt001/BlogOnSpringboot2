51a7d5dd1cca248f1949ea342e7c635a7726d7e2
aihaitt001 <djb19970120@gmail.com>
2018-09-29 15:13:30 +0800

    新增静态页面admin,allArticles,article,contactMe.

--- src/main/java/cn/djb/springboot2/controller/HomeController.java
+++ src/main/java/cn/djb/springboot2/controller/HomeController.java
@@ -6,11 +6,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,7 +20,7 @@ public class HomeController {
 
     @GetMapping("/home")
     public ModelAndView home(ModelAndView mav) {
-        mav.setViewName("home");
+        mav.setViewName("article");
         return mav;
     }
     @GetMapping("/getHomeArticles")
--- src/main/java/cn/djb/springboot2/util/JsonUtil.java
+++ src/main/java/cn/djb/springboot2/util/JsonUtil.java
@@ -1,26 +1,42 @@
 package cn.djb.springboot2.util;
 
 
+import cn.djb.springboot2.domain.BlogUser;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+/*
+*@Decription:利用jackson.core 进行json字符串和对象的相互转化
+*@Classname:JsonUtil
+*@Author: ovo
+*@Date:2018/9/28 15:46
+*/
 
 public class JsonUtil {
 
-    private final static ObjectMapper objMapper = new ObjectMapper();
+    private final static ObjectMapper OBJMAPPER = new ObjectMapper();
 
     static {
-        objMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
-        objMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        objMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        objMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true); /* see: http://jira.codehaus.org/browse/JACKSON-208 */
+        OBJMAPPER.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+        OBJMAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        OBJMAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        OBJMAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true); /* see: http://jira.codehaus.org/browse/JACKSON-208 */
     }
 
-    public static String serialize(Object obj) {
+    public static String toString(Object obj) {
         try {
-            return objMapper.writeValueAsString(obj);
+            return OBJMAPPER.writeValueAsString(obj);
         } catch (Exception e) {
             e.printStackTrace();
-            return "serialize出错"+e.toString();
+            return "serialize出错" + e.toString();
+        }
+    }
+
+    public static Object toObject(String strJson, Class objClass) {
+        try {
+            return OBJMAPPER.readValue(strJson, objClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "" + e.toString();
         }
     }
 
